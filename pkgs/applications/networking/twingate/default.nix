ace10762e07ed432d17235021e864ed2f3880c4a
Alex Chupryna <ochupryna@twingate.com>
2022-08-30 11:36:03 +0300

    bumped twingate to 1.0.60

--- pkgs/applications/networking/twingate/default.nix
+++ pkgs/applications/networking/twingate/default.nix
@@ -13,11 +13,11 @@
 
 stdenv.mkDerivation rec {
   pname = "twingate";
-  version = "1.0.58";
+  version = "1.0.60";
 
   src = fetchurl {
     url = "https://binaries.twingate.com/client/linux/DEB/${version}/twingate-amd64.deb";
-    sha256 = "b7010f65fa2efa1fda87b24fbe847084492415423aebcd1ba16addeb7cb243cb";
+    sha256 = "b308c422af8a33ecd58e21a10a72c353351a189df67006e38d1ec029a93d5678";
   };
 
   buildInputs = [ curl libnl udev cryptsetup ];
